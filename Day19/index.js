1dd4790b78ec3f5a2f759a85a5ef268925e27d33
adaskruk <adam.krukiewicz@gmail.com>
2025-06-13 22:55:41 +0200

    day19 done

--- Day19/index.js
+++ Day19/index.js
@@ -1,19 +1,33 @@
 "use strict";
 
+//Tab button click handler
 const tabs = document.querySelector(".tab-headers");
 tabs.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
     switchToTab(e.target.getAttribute("data-tab"));
   }
 });
+document.addEventListener('tab-switched', e => {
+  console.log('Tab clicked:', e.detail.tabNr, e.detail.activeTab);
+})
 const btns = document.getElementsByClassName("tab");
-console.log(btns);
 const contents = document.getElementsByClassName("content");
 
 function switchToTab(tabNr) {
-  console.log("Clicked Tab:", tabNr);
   [...btns].forEach((btn) => {
-    btn.classList.toggle("active");
+    btn.classList.toggle("active", btn.getAttribute('data-tab') === tabNr);
   });
+  [...contents].forEach((content) => {
+    content.classList.toggle('active', content.getAttribute('data-tab') === tabNr)
+  });
+  //Custom Event implementation
+  const activeTab = document.querySelector('.tab.active').textContent;
+  const event = new CustomEvent('tab-switched', {detail: {tabNr, activeTab}});
+  document.dispatchEvent(event);
 }
-//keyboard shortcuts
+//keyboard shortcuts (copied from task)
+document.addEventListener("keydown", (e) => {
+    if (e.key === "1") switchToTab('1');
+    if (e.key === "2") switchToTab('2');
+    if (e.key === "3") switchToTab('3');
+    });
